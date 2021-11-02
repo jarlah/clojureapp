b86d4be2ffb8eae5dc15aa04473538108f9ef940
Jarl André (Private) <jarlah@protonmail.com>
2021-11-02 17:46:38 +0100

    fix

--- src/untitled/core.clj
+++ src/untitled/core.clj
@@ -2,8 +2,11 @@
   (:require [clj-http.client :as client])
   (:require [cheshire.core :refer :all]))
 
+(def base-url "https://esi.evetech.net/latest")
+
 (defn get-market-orders [region-id]
-  (client/get (str "https://esi.evetech.net/latest/markets/" region-id "/orders") {:accept :json :as :json}))
+  (client/get (str base-url "/markets/" region-id "/orders")
+              {:accept :json :as :json :throw-exceptions false}))
 
 (defn get-unique-type-ids [orders]
   (-> (map :type_id orders)
@@ -11,18 +14,24 @@
 
 (defn get-universe-objects
   [type-ids]
-  (client/post "https://esi.evetech.net/latest/universe/names" {:body (generate-string type-ids) :as :json :content-type :json}))
+  (client/post (str base-url "/universe/names")
+               {:body (generate-string type-ids) :as :json :content-type :json :throw-exceptions false}))
 
 (defn get-unique-object-names-sorted [objects]
   (-> (map :name objects)
       (distinct)
       (sort)))
 
+(defn handle_response [response]
+  (if (== 200 (:status response))
+    (:body response)
+    (println (str "Request failed with status code " (:status response) ": " + (:body response)))))
+
 (defn -main []
   (some-> (get-market-orders 10000002)
-          (:body)
+          (handle_response)
           (get-unique-type-ids)
           (get-universe-objects)
-          (:body)
+          (handle_response)
           (get-unique-object-names-sorted)
           (println)))
